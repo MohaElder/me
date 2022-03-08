7d5c1e6d7b83fb6e46aa4e999741e7872088e0a9
MohaElder <calen0909@hotmail.com>
2022-03-07 22:13:54 -0800

    change init.py

--- helpers/init.py
+++ helpers/init.py
@@ -1,13 +1,13 @@
 import os
-path = 'src/images'
+path = '../src/images'
 files = os.listdir(path)
 str = "const images = ["
 for file in files:
     if os.path.isfile(os.path.join(path, file)):
-        str += 'require("../images/' + file + '"),'
+        str += 'require("https://github.com/MohaElder/me/raw/main/src/images/' + file + '"),'
 str += "] \n export { images };"
 
-with open('src/utils/imageLink.js', "w") as myfile:
+with open('../src/utils/imageLink.js', "w") as myfile:
     myfile.write(str)
 
 print("Update Complete!")
