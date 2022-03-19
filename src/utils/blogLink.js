38fc4ca7efe2d918331086ece04037016e7c69f5
MohaElder <calen0909@hotmail.com>
2022-03-18 21:52:12 -0700

    write blog

--- src/utils/blogLink.js
+++ src/utils/blogLink.js
@@ -1,11 +1,11 @@
-const blogs = {"7d81c707a73711ecb6f768545a57002a": {
+const blogs = {"4b844beea74011ec9f0e68545a57002a": {
                 color:"#64b5f6",
                 img: "https://github.com/MohaElder/me/raw/main/src/assets/blogPics/devLogCover.png",
                 title: "Development Log of this website",
                 brief: "This blog notes the development progress of this website, including why this website exists and how does it become what you see today.",
                 article: "https://raw.githubusercontent.com/MohaElder/me/main/src/blogs/DevLog.md",
                 },
-                "7d81c708a73711ecaabe68545a57002a": {
+                "4b84730ba74011ec80ac68545a57002a": {
                 color:"#64b5f6",
                 img: "https://mdg.imgix.net/assets/images/book-cover.jpg?auto=format&fit=clip&q=40&w=1080",
                 title: "Sample Title",
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -2,7 +2,7 @@
   <v-container style="color: white">
     <v-parallax dark :src="blog.img">
       <v-row align="center" justify="center">
-        <v-col class="text-center" cols="12">
+        <v-col class="text-center" cols="12" style="background-color: rgba(25, 25, 25, 0.45);">
           <h1>{{ blog.title }}</h1>
           <h2>{{ blog.brief }}</h2>
         </v-col>
