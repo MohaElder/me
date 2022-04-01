fd6f285815d91c3047f8e0a687b1a1d913943f1b
MohaElder <calen0909@hotmail.com>
2022-03-31 21:04:36 -0700

    new article

--- src/utils/blogLink.js
+++ src/utils/blogLink.js
@@ -14,5 +14,13 @@ const blogs = {"b33c27e1a98011ecbeb368545a57002a": {
                 date: "2022/3/19",
                 article: "https://raw.githubusercontent.com/MohaElder/me/main/src/blogs/DevLog.md",
                 },
+                "bb309f2fb17011ecbef668545a57002a": {
+                color:"#64b5f6",
+                img: "https://github.com/MohaElder/me/raw/main/src/assets/blogPics/grape_juice/grape_cover.JPG",
+                title: "[鸡酒分享] 玉光葡汁",
+                brief: "分享一款之前有一天喝酒即兴调的还不错的配方，自由度较高。",
+                date: "2022/3/31",
+                article: "https://raw.githubusercontent.com/MohaElder/me/main/src/blogs/jade_grape_juice.md",
+                },
                 } 
  export { blogs };
