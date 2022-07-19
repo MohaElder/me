2b98a02df047c9d113339337fe99aa299082ec36
MohaElder169 <calen0909@hotmail.com>
2022-07-18 23:47:04 -0700

    new images and todos

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -1,4 +1,22 @@
 <template>
+<!--
+  TODO: A webgl based image viewer
+  1. Video Background + 3d Foreground
+  2. 3D Image Frame, frame changes based on different image aspect ratio
+  3. Interactive, can switch image
+
+  Should have multiple scenes in the future
+
+  Scene1: Gallery
+  Scene2: Sea
+
+  Scene1: Gallery
+  Background: White Wall <a href='https://www.freepik.com/photos/grey-paper'>Grey paper photo created by rawpixel.com - www.freepik.com</a>
+  Foreground: Image Frame respective to different image aspect ratio "3D Architecture | Photo frame" (https://skfb.ly/6SsGS) by Dylan Dreyer Varsics is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+  Light: Spotlight from upper right angle
+  kaideng guandeng + yinxiao jiaohu
+  
+-->
   <v-container>
     <v-overlay :value="previewPic">
       <v-img
