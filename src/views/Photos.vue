db342764e1f0575c126749092c8e89b12d1a6aa2
MohaElder <calen0909@hotmail.com>
2022-07-29 17:39:44 -0700

    update unity

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -22,19 +22,20 @@
         :max-height="$vuetify.breakpoint.height * 0.8" contain @click="previewPic = false"></v-img>
       <v-btn style="margin-top: 10px" @click="download">download</v-btn>
     </v-overlay>
-    <v-btn @click="unity = !unity">switch</v-btn>
-    <span v-if="unity">
-      <v-row justify="center" style="padding: 30px">
-        <p style="width: 100%; text-align: start">
-          <b>USING MY PHOTO</b>
-          <br>
-          non-commercial: Please give credit to the name "Yasushi Oh" or
-          "Anzhi Weng" or "翁安志" or "お やすし" (your choice :))
-          <br>
-          <a style="color: white"
-            href="mailto:calen0909@hotmail.com?subject=Photo Commercial Usage Request&body=(Thank you for showing interest in my photo! Please address your usage and attach the photo that you want to use)">commercial</a>
-        </p>
-      </v-row>
+    <v-row justify="center" style="padding: 30px">
+      <p style="width: 100%; text-align: start">
+        <b>USING MY PHOTO</b>
+        <br>
+        non-commercial: Please give credit to the name "Yasushi Oh" or
+        "Anzhi Weng" or "翁安志" or "お やすし" (your choice :))
+        <br>
+        <a style="color: white"
+          href="mailto:calen0909@hotmail.com?subject=Photo Commercial Usage Request&body=(Thank you for showing interest in my photo! Please address your usage and attach the photo that you want to use)">commercial</a>
+      </p>
+    </v-row>
+    <v-switch v-model="unity" label="Gallery Mode"></v-switch>
+    <span v-if="!unity">
+
       <v-row justify="center" style="padding: 30px" class="d-flex child-flex">
         <v-col style="display: flex; width: 50%; justify-content: center" v-for="img in images" v-bind:key="img">
           <v-img :aspect-ratio="5 / 4" :width="500" :src="img" contain @click="showPic(img)"></v-img>
@@ -42,10 +43,8 @@
       </v-row>
     </span>
     <span v-else>
-      <!-- <canvas id="unity-canvas" width=960 height=600 style="width: 960px; height: 600px; background: #1F1F20"></canvas> -->
-      <div style="width: 800px; height: 600px; border: 1px solid #f00">
-        <canvas id="canvas" ref="canvas" style="width: 100%; height: 100%" />
-      </div>
+      <iframe mozallowfullscreen="true" src="https://mohaelder.github.io/Gallery/" scrolling="no" frameborder="0"
+        :width="1920 + 'px'" :height="1080 + 'px'"></iframe>
     </span>
 
   </v-container>
@@ -53,31 +52,12 @@
 
 <script>
 import { images } from "../utils/imageLink.js";
-import UnityWebgl from 'unity-webgl'
 
 export default {
   name: "Photos",
-
-  components: {
-
-  },
-
   mounted() {
     this.images = images;
-
-    let UnityContext = new UnityWebgl('#canvas', {
-      loaderUrl: "https://cdn.jsdelivr.net/gh/MohaElder/me/src/assets/gallery/Build/Output.loader.js",
-      dataUrl: "https://cdn.jsdelivr.net/gh/MohaElder/me/src/assets/gallery/Build/Output.data",
-      frameworkUrl: "https://cdn.jsdelivr.net/gh/MohaElder/me/src/assets/gallery/Build/Output.framework.js",
-      codeUrl: "https://cdn.jsdelivr.net/gh/MohaElder/me/src/assets/gallery/Build/Output.wasm",
-    })
-
-    this.$once('hook:beforeDestroy', function () {
-      UnityContext.destroy()
-      UnityContext = null
-    })
   },
-
   data: () => ({
     images: ["../images/_DSF4726.jpg"],
     previewPic: false,
