416043f68a8f6cf0bd8fe1eafdb6940a2449854c
MohaElder <calen0909@hotmail.com>
2024-11-14 18:56:46 -0800

    feat: update work page

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,12 +1,11 @@
-
-<template >
+<template>
   <v-container>
-    <h3 class="text-left" :style="$vuetify.display.mobile ? 'font-size: 7.8vw;' : 'font-size: 5vh'">
+    <h3 style="padding-top: 7%;" class="text-left" :style="$vuetify.display.mobile ? 'font-size: 7.8vw;' : 'font-size: 5vh'">
       {{ $t("message.work.a") }}
       <a href="https://ucsd.edu/" target="_blank"><i>UCSD</i></a>{{ $t("message.work.b")
       }}<a href="https://aako.world/" target="_blank"><i>Aako</i></a>{{ $t("message.work.c")
       }}<a href="https://www.unops.org/" target="_blank"><i>UN</i></a>{{ $t("message.and")
-}}<a href="https://www.nvidia.com/" target="_blank"><i>NVIDIA</i></a>{{ $t("message.work.d") }}
+      }}<a href="https://www.nvidia.com/" target="_blank"><i>NVIDIA</i></a>{{ $t("message.work.d") }}
     </h3>
     <v-img style="
         margin-top: 5%;
@@ -14,12 +13,13 @@
         box-shadow: rgba(255, 255, 255, 0.15) 0px 48px 100px 0px;
       " :style="$vuetify.display.mobile ? 'width: 100%;' : 'width: 35%;'" src="../assets/profile_pic.jpg"
       cover></v-img>
-    <h1 class="big-title">{{ $t("message.resume") }}</h1>
-    <v-btn size="x-large" variant="outlined" color="#ffd738"
-      href="https://github.com/MohaElder/me/raw/main/src/assets/resume.pdf" style="margin-left: 5px">{{
-        $t("message.download") }}</v-btn>
-    <v-btn v-if="$i18n.locale == 'zh'" size="x-large" variant="outlined" color="#ffd738"
-      href="https://github.com/MohaElder/me/raw/main/src/assets/resume_cn.pdf" style="margin-left: 5px">中文简历</v-btn>
+    <div style="padding-top: 5%;">
+      <v-btn size="x-large" variant="outlined" color="#ffd738"
+        href="https://github.com/MohaElder/me/raw/main/src/assets/resume.pdf" style="margin-left: 5px">{{
+          $t("message.download") }}</v-btn>
+      <v-btn v-if="$i18n.locale == 'zh'" size="x-large" variant="outlined" color="#ffd738"
+        href="https://github.com/MohaElder/me/raw/main/src/assets/resume_cn.pdf" style="margin-left: 5px">中文简历</v-btn>
+    </div>
     <v-row style="padding-left: 15px" justify="space-between">
       <v-col>
         <v-row style="margin-top: 50px">
@@ -35,45 +35,45 @@
             </v-row>
             <v-row :style="$vuetify.display.mobile ? 'margin-top: 15%' : 'margin-top: 5%'
               ">
-              <h4>TypeScript, C#,</h4>
+              <h4>C#, TypeScript</h4>
             </v-row>
             <v-row>
-              <h4>Python, Swift, Java</h4>
+              <h4>Swift, Python, Java</h4>
             </v-row>
             <v-row>
-              <h4>Lua, C++, C, ARM</h4>
+              <h4>Lua, C++</h4>
             </v-row>
             <span v-if="$vuetify.display.mobile"><v-row style="margin-top: 15%">
-                <h4>SQL, STL, DevOps, Unit Testing,</h4>
+                <h4>DevOps, Database, Design Pattern, Testing</h4>
               </v-row>
               <v-row>
-                <h4>Domain-Driven Design, Model-View-ViewModel,</h4>
+                <h4>Entrepreneurship, Project Management</h4>
               </v-row>
               <v-row style="margin-top: 15%">
-                <h4>User Research, UI</h4>
+                <h4>User Research, UI/UX</h4>
               </v-row>
               <v-row>
-                <h4>Unity3D, Wwise</h4>
+                <h4>Video Game Production</h4>
               </v-row>
               <v-row>
-                <h4>Ps, Pr, Ai, Ae, Lr, Id</h4>
+                <h4>Photography, Cinematography</h4>
               </v-row></span>
           </v-col>
           <v-col v-if="!$vuetify.display.mobile">
             <v-row>
-              <h4>SQL, STL, DevOps, Unit Testing,</h4>
+              <h4>DevOps, Database, Design Pattern, Testing</h4>
             </v-row>
             <v-row>
-              <h4>Domain-Driven Design, Model-View-ViewModel,</h4>
+              <h4>Entrepreneurship, Project Management</h4>
             </v-row>
             <v-row style="margin-top: 5%">
-              <h4>User Research, UI</h4>
+              <h4>User Research, UI/UX</h4>
             </v-row>
             <v-row>
-              <h4>Unity3D, Wwise</h4>
+              <h4>Video Game Production</h4>
             </v-row>
             <v-row>
-              <h4>Ps, Pr, Ai, Ae, Lr, Id</h4>
+              <h4>Photography, Cinematography</h4>
             </v-row>
           </v-col>
         </v-row>
@@ -84,7 +84,7 @@
         </v-row>
         <v-row>
           <h4>
-            <a href="mailto:yoh@ucsd.edu">yoh@ucsd.edu</a>
+            <a href="mailto:calen0909@hotmail.com">calen0909@hotmail.com</a>
           </h4>
         </v-row>
         <v-row>
@@ -130,6 +130,22 @@
         </v-row>
         <v-row>
           <h4>
+            {{ $t("message.great_stuff_desc.ucla_before") }}
+            {{ $t("message.great_stuff_desc.ucla") }}
+            {{ $t("message.great_stuff_desc.ucla_after") }}
+          </h4>
+        </v-row>
+        <v-row>
+          <h4>
+            {{ $t("message.great_stuff_desc.nsf_before") }}
+            <a href="https://new.nsf.gov/funding/initiatives/i-corps" target="_blank">{{
+              $t("message.great_stuff_desc.nsf")
+            }}</a>
+            {{ $t("message.great_stuff_desc.nsf_after") }}
+          </h4>
+        </v-row>
+        <v-row>
+          <h4>
             {{ $t("message.great_stuff_desc.eroducate_before") }}
             <a href="https://www.eroducate.xyz/" target="_blank">{{
               $t("message.great_stuff_desc.eroducate_link")
@@ -160,6 +176,15 @@
         </v-row>
         <v-row>
           <h4>
+            {{ $t("message.work_experience_desc.avian_enigma_left") }}
+            <a href="https://store.steampowered.com/app/2784960/_/" target="_blank">{{
+              $t("message.work_experience_desc.avian_enigma")
+            }}</a>
+            {{ $t("message.work_experience_desc.avian_enigma_right") }}🐧
+          </h4>
+        </v-row>
+        <v-row>
+          <h4>
             {{ $t("message.work_experience_desc.aako_left") }}
             <a href="https://aako.world/" target="_blank">{{
               $t("message.work_experience_desc.aako")
@@ -183,6 +208,13 @@
               $t("message.work_experience_desc.ucsd")
             }}</a>
             {{ $t("message.work_experience_desc.ucsd_right") }}
+          </h4>
+        </v-row>
+        <v-row>
+          <h4>
+            {{ $t("message.work_experience_desc.esailab_left") }}
+            {{ $t("message.work_experience_desc.esailab") }}
+            {{ $t("message.work_experience_desc.esailab_right") }}
           </h4>
         </v-row>
         <v-row>
@@ -284,6 +316,13 @@
         <v-row style="margin-top: 50px; padding-left: 15px">
           <h1>{{ $t("message.podcast") }}</h1>
         </v-row>
+        <v-row style="padding-left: 15px">
+          <h4><a
+              href="https://medium.com/@calen0909/implementing-semantic-search-with-openai-postgres-and-entity-framework-4414cf516b48"
+              target="_blank">Implementing Semantic Search with OpenAI, Postgres, and Entity Framework</a></h4>
+          <h4><a href="https://medium.com/@calen0909/why-dont-i-like-oas-how-to-design-a-good-oa-ac2716a71682"
+              target="_blank">Why don't I like OAs & how to design a good OA</a></h4>
+        </v-row>
         <iframe id="embedPlayer"
           src="https://embed.podcasts.apple.com/us/podcast/randoms-hit-radio-vol-0-tgif%E7%89%B9%E8%BE%91-%E8%8A%82%E5%BA%86%E6%96%87%E5%8C%96-%E7%AC%A6%E5%8F%B7%E5%AD%A6-%E9%9F%B3%E4%B9%90%E9%89%B4%E8%B5%8F-%E5%93%B2%E5%AD%A6-%E9%9A%8F%E4%BE%BF%E8%81%8A/id1694128032?i=1000618220058&amp;itsct=podcast_box_player&amp;itscg=30200&amp;ls=1&amp;theme=auto"
           height="175px" frameborder="0"
@@ -304,11 +343,6 @@
 </template>
 
 <style scoped>
-.big-title {
-  padding-top: 5%;
-  font-size: 5vh;
-}
-
 h4 {
   font-weight: 400;
 }
@@ -330,4 +364,3 @@ export default {
   },
 };
 </script>
-
