a7781e5e65dae8817dbb9f707fb8491d3be934fd
MohaElder <calen0909@hotmail.com>
2025-03-11 15:21:35 -0700

    feat: update press paper

--- src/views/LandEmbodied.vue
+++ src/views/LandEmbodied.vue
@@ -1,38 +1,10 @@
 <template>
   <v-container>
-    <h1>LAND EMBODIED <br> <span style="font-size: 16px">presented by Commons Gallery</span></h1>
+    <h1>LAND EMBODIED <br> <span style="font-size: 16px">presented by SME Gallery</span></h1>
     <div style="padding-top: 10px; padding-bottom: 10px"> <p>
-      <i>This work is presented to you by Dmitri Zurita's students. He has encouraged us, inspired us, and supported
-        us every step of the way.</i>
+      <i>This work is presented to you by Lorena Mostajo students. She has encouraged us, inspired us, and supported us every step of the way.</i>
     </p>
-    <p>"I'm deeply moved by the work produced by these young artists. They've shed their self imposed limitations and
-      in that process found the voice and a beautiful community." - Dmitri Zurita</p></div>
-   
-    <h2>Epilogue</h2>
-    <p class="first-letter"> People love to gamble — whether it's in casinos, betting with friends, or even making a personal wager on
-      whether the traffic light will turn green at the next beat drop of the song you're listening to. The reason?
-      People thrive on the thrill of winning. That feeling is magnified when you go all in, when you have nothing left
-      to lose. We often suppress this desire, but deep down, we all wish to be that protagonist in the movies who bets
-      everything they have and emerges victorious. Even if we don't win, the rush, the surge of adrenaline, is
-      intoxicating—like a wild mustang running naked through the Mojave Desert.</p>
-    <p>In this show, we present you a series of works that expresses this "all or nothing" feeling originating from the
-      artists' most vulnerable parts. You will see violence in action, naked bodies, and all the other explicit content
-      that one might interpret as an R-rated movie. If you see it that way, it's ok. Because that is the way many of the
-      artists in this exhibition might have seen these works at the start of this school year. People create art for
-      reasons, even when it comes to an art with no reasons, that is also a reason. The artists of this exhibition have
-      all gone through an inspiring and explicit journey that prompts each to discover and challenge one's most
-      vulnerable spots. These journeys include getting your work constantly commented as boring, making out in the
-      darkroom, almost losing your professor's trust because of some stupid high-school vibe thing, caging yourself
-      inside a dog cage, being a joker, running naked covered with blood in the forest, carving yourself, and more.</p>
-    <p>We invite you, as you explore the works in this exhibition, to immerse yourself in these journeys and to find
-      echoes of your own vulnerabilities within them. It is a treacherous and uncomfortable trip to delve into these
-      deepest scars of your life. Yet, if you dare to face those scars and express them, you may find yourself
-      omniscient about your own existence and even omnipotent in other aspects of your life—or this world. As an old
-      Chinese quote says, "知人者智，自知者明 (A truly strong person has no fear of exposing their weakest points)" It is our
-      hope that everyone who visits this exhibition will emerge as a better version of themselves, evolving through this
-      experience. In the end, self-hatred and nihilism cage us, but only love sets us free and carries us above and
-      beyond. We are young, and we have nothing to lose. Even if you're old, you still have nothing to lose when you're
-      ready to take your emotions on an adrenaline trip that gives you a head rush via a journey into vulnerability.</p>
+    </div>
 
     <h2>Featured Artists</h2>
     <ul style="padding-left: 20px;">
@@ -64,13 +36,22 @@
 
     <div class="works">
       <v-row>
-        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/9S4A3496 - Evan Wu.jpeg"></v-img></v-col>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/IMG_6245.png"></v-img></v-col>
         <v-col>
-          <h3>Tre Cime di Lavaredo</h3>
-          <p>By Evan Wu <a href="https://www.instagram.com/evanwuu/">@evanwuu</a></p>
+          <h3>Never Forget This</h3>
+          <p>By Nataly Pastukhov <a href="https://www.instagram.com/nats.lenses/">@nats.lenses </a></p>
+          <p>By Gabriella Crisa <a href="https://www.instagram.com/fuzzzyfotos/">@fuzzzyfotos</a></p>
+          <p>Our work was originally inspired by the early works of Ryan McGinley, however along the way we found our own style of art through it. We go out, break norms, have fun, and capture it. </p>
         </v-col>
       </v-row>
-
+      <v-row>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/DSC04761 2 Large - Joshua Chew.jpeg"></v-img></v-col>
+        <v-col>
+          <h3>Slam Jam</h3>
+          <p>By Joshua Chew <a href="https://www.instagram.com/josh.cheww/">@josh.cheww</a></p>
+          <p>Jasen Brooks, UCSD MBB Redshirt Freshman Guard, rises for the one-handed slam during warmups</p>
+        </v-col>
+      </v-row>
       <v-row>
         <v-col><v-img class="square-img" cover src="../assets/landEmbodied/9S4A2760 - Evan Wu.jpeg"></v-img></v-col>
         <v-col>
@@ -78,7 +59,6 @@
           <p>By Evan Wu <a href="https://www.instagram.com/evanwuu/">@evanwuu</a></p>
         </v-col>
       </v-row>
-
       <v-row>
         <v-col><v-img class="square-img" cover src="../assets/landEmbodied/128A4651 - Shiv Bhardwaj.jpeg"></v-img></v-col>
         <v-col>
@@ -86,7 +66,44 @@
           <p>By Shiv Bhardwaj <a href="https://www.instagram.com/shivsocial/">@shivsocial</a></p>
         </v-col>
       </v-row>
-
+      <v-row>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/9S4A3496 - Evan Wu.jpeg"></v-img></v-col>
+        <v-col>
+          <h3>Tre Cime di Lavaredo</h3>
+          <p>By Evan Wu <a href="https://www.instagram.com/evanwuu/">@evanwuu</a></p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/IMG_8377 - Reed Solbach.jpeg"></v-img></v-col>
+        <v-col>
+          <h3>Subway Strangers</h3>
+          <p>By Reed Solbach <a href="https://www.instagram.com/reed_solbach/">@reed_solbach</a></p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/IMG_8379 - Ryan Giordano.jpeg"></v-img></v-col>
+        <v-col>
+          <h3>The Edge of Stillness</h3>
+          <p>By Ryan Giordano <a href="https://www.instagram.com/ryangiordanophotographyco/">@ryangiordanophotographyco</a></p>
+          <p>This photograph was shot in The Blue Lagoon located in the island of Malta in the Mediterranean Sea in the fall of 2023. The Edge of Stillness shows party goers on a day cruise which meets the waters quiet and still waters of the Blue Lagoon. Then the edge which is the separation of the quiet waters and the party goers get broken, the party goers are then in the Lagoon. This then embodies them in the environment of the Blue Lagoon surrounded by the cliffs and the sea life in the Lagoon.</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/8EB928A9-54B2-4061-A7B7-CCB010F9832E - Leopoldo Noriega.jpeg"></v-img></v-col>
+        <v-col>
+          <h3>Día del los Muertos</h3>
+          <p>By Leo Noriega <a href="https://www.instagram.com/photo.book_1/">@photo.book_1</a></p>
+          <p>Inspired by Día del los Muertos traditional clothing and face paint, I decided to recreate it to show my appreciation of my culture.</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/20250310_222110 - Kirsten Yen.jpg"></v-img></v-col>
+        <v-col>
+          <h3>Ghostlight</h3>
+          <p>By Kirsten Yen <a href="https://www.instagram.com/kirsten_makes_stuff/">@kirsten_makes_stuff</a></p>
+          <p>This work is part of a series of five photographs that explores the facade of American suburbia and the deconstruction of that through photographs.</p>
+        </v-col>
+      </v-row>
       <v-row>
         <v-col><v-img class="square-img" cover src="../assets/landEmbodied/12 - Erqian Sun.jpg"></v-img></v-col>
         <v-col>
@@ -95,13 +112,36 @@
           <p>Paris, France, 2024. Les Parisiens captures a fleeting moment of Paris urban life, where strangers share space yet remain disconnected. A subtle hand gesture hints at hidden intentions, inviting the viewer to interpret the quiet tension within the everyday scene.</p>
         </v-col>
       </v-row>
+      <v-row>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/20250310194844 - Jiukan Li.jpg"></v-img></v-col>
+        <v-col>
+          <h3>Obelisks</h3>
+          <p>By Jiukan Terry Li <a href="https://www.instagram.com/tliphotography_/">@tliphotography_</a></p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/IMG_8287.JPG"></v-img></v-col>
+        <v-col>
+          <h3>OVERTIME HOURS</h3>
+          <p>By Eric Cai</p>
+          <p>Printing screen on the left. Shirt printed with silver gelatin photo emulsion on the right. All made from photos from image archives. </p>
+        </v-col>
+      </v-row>
 
       <v-row>
-        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/IMG_8379 - Ryan Giordano.jpeg"></v-img></v-col>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/Ashley_Wu_Gallery - Ashley Wu.png"></v-img></v-col>
         <v-col>
-          <h3>The Edge of Stillness</h3>
-          <p>By Ryan Giordano <a href="https://www.instagram.com/ryangiordanophotographyco/">@ryangiordanophotographyco</a></p>
-          <p>This photograph was shot in The Blue Lagoon located in the island of Malta in the Mediterranean Sea in the fall of 2023. The Edge of Stillness shows party goers on a day cruise which meets the waters quiet and still waters of the Blue Lagoon. Then the edge which is the separation of the quiet waters and the party goers get broken, the party goers are then in the Lagoon. This then embodies them in the environment of the Blue Lagoon surrounded by the cliffs and the sea life in the Lagoon.</p>
+          <h3>The Little Red Book, Darling, The Yellow Wallpaper</h3>
+          <p>By Ashley Wu <a href="https://www.instagram.com/ashleywu_photography/">@ashleywu_photography</a></p>
+          <p>
+          These three collages are made up of archival images of magazines, books, advertisements, and various paintings. Inspired by scrapbooks and junk journals, these digital collages poke fun at the culture of sourcing materials for the aesthetic without the outside context.</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/IMG_2522 - Gabriella Crisa.jpeg"></v-img></v-col>
+        <v-col>
+          <h3>The kiss of life</h3>
+          <p>By Gabriella Crisa <a href="https://www.instagram.com/fuzzzyfotos/">@fuzzzyfotos</a></p>
         </v-col>
       </v-row>
 
@@ -128,33 +168,6 @@
           <p>By Blake Riesenfeld <a href="https://www.instagram.com/Blakeries_/">@Blakeries_</a></p>
         </v-col>
       </v-row>
-
-      <v-row>
-        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/20250310194844 - Jiukan Li.jpg"></v-img></v-col>
-        <v-col>
-          <h3>Obelisks</h3>
-          <p>By Jiukan Terry Li <a href="https://www.instagram.com/tliphotography_/">@tliphotography_</a></p>
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/8EB928A9-54B2-4061-A7B7-CCB010F9832E - Leopoldo Noriega.jpeg"></v-img></v-col>
-        <v-col>
-          <h3>Día del los Muertos</h3>
-          <p>By Leo Noriega <a href="https://www.instagram.com/photo.book_1/">@photo.book_1</a></p>
-          <p>Inspired by Día del los Muertos traditional clothing and face paint, I decided to recreate it to show my appreciation of my culture.</p>
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/PXL_20250311_051213580 - Payton Martinez.jpg"></v-img></v-col>
-        <v-col>
-          <h3>Gaze</h3>
-          <p>By Payton <a href="https://www.instagram.com/c0mfortcruise/">@c0mfortcruise</a></p>
-          <p>A reflection of the self, where the viewer is forced into a state of mind made to replicate my own struggle with self-loathing. Shot on Ilford HP5 35mm, scanned and digitally printed.</p>
-        </v-col>
-      </v-row>
-
       <v-row>
         <v-col><v-img class="square-img" cover src="../assets/landEmbodied/DSC07745 - Kelly Tapia.jpeg"></v-img></v-col>
         <v-col>
@@ -171,15 +184,14 @@
           <p>By Kelly Tapia <a href="https://www.instagram.com/kellyt_art/">@kellyt_art</a></p>
         </v-col>
       </v-row>
-
       <v-row>
-        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/IMG_8377 - Reed Solbach.jpeg"></v-img></v-col>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/PXL_20250311_051213580 - Payton Martinez.jpg"></v-img></v-col>
         <v-col>
-          <h3>Subway Strangers</h3>
-          <p>By Reed Solbach <a href="https://www.instagram.com/reed_solbach/">@reed_solbach</a></p>
+          <h3>Gaze</h3>
+          <p>By Payton <a href="https://www.instagram.com/c0mfortcruise/">@c0mfortcruise</a></p>
+          <p>A reflection of the self, where the viewer is forced into a state of mind made to replicate my own struggle with self-loathing. Shot on Ilford HP5 35mm, scanned and digitally printed.</p>
         </v-col>
       </v-row>
-
       <v-row>
         <v-col><v-img class="square-img" cover src="../assets/landEmbodied/exhibitionPhoto1 - Sarai Nava.jpg"></v-img></v-col>
         <v-col>
@@ -187,43 +199,13 @@
           <p>By Sarai Nava <a href="https://www.instagram.com/dinosaraii/">@dinosaraii</a></p>
         </v-col>
       </v-row>
-
       <v-row>
-        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/Ashley_Wu_Gallery - Ashley Wu.png"></v-img></v-col>
+        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/IMG_6244.png"></v-img></v-col>
         <v-col>
-          <h3>The Little Red Book, Darling, The Yellow Wallpaper</h3>
-          <p>By Ashley Wu <a href="https://www.instagram.com/ashleywu_photography/">@ashleywu_photography</a></p>
-          <p>
-          These three collages are made up of archival images of magazines, books, advertisements, and various paintings. Inspired by scrapbooks and junk journals, these digital collages poke fun at the culture of sourcing materials for the aesthetic without the outside context.</p>
+          <h3>Я Україна</h3>
+          <p>By Nataly Pastukhov <a href="https://www.instagram.com/nats.lenses/">@nats.lenses </a></p>
         </v-col>
       </v-row>
-
-      <v-row>
-        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/DSC04761 2 Large - Joshua Chew.jpeg"></v-img></v-col>
-        <v-col>
-          <h3>Slam Jam</h3>
-          <p>By Joshua Chew <a href="https://www.instagram.com/josh.cheww/">@josh.cheww</a></p>
-          <p>Jasen Brooks, UCSD MBB Redshirt Freshman Guard, rises for the one-handed slam during warmups</p>
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/20250310_222110 - Kirsten Yen.jpg"></v-img></v-col>
-        <v-col>
-          <h3>Ghostlight</h3>
-          <p>By Kirsten Yen <a href="https://www.instagram.com/kirsten_makes_stuff/">@kirsten_makes_stuff</a></p>
-          <p>This work is part of a series of five photographs that explores the facade of American suburbia and the deconstruction of that through photographs.</p>
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col><v-img class="square-img" cover src="../assets/landEmbodied/IMG_2522 - Gabriella Crisa.jpeg"></v-img></v-col>
-        <v-col>
-          <h3>The kiss of life</h3>
-          <p>By Gabriella Crisa <a href="https://www.instagram.com/fuzzzyfotos/">@fuzzzyfotos</a></p>
-        </v-col>
-      </v-row>
-
     </div>
 
   </v-container>
