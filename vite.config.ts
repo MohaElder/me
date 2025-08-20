c7cc971a166b63d82f7bedd1ef702ce89842ef22
MohaElder <calen0909@hotmail.com>
2025-08-20 15:42:31 -0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/me/' : '/me',
+  base: '/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
